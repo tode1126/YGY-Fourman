240a2dc34b21670b8fe6dac66fc9ce727981e020
Hong <tode1126@gmail.com>
2019-07-04 16:15:05 +0900

    git ignore add

---
+++ Fourman-YGY-Project/src/main/java/spring/data/restraunt/RestrauntDao.java
@@ -0,0 +1,9 @@
+package spring.data.restraunt;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RestrauntDao extends SqlSessionDaoSupport {
+
+}
