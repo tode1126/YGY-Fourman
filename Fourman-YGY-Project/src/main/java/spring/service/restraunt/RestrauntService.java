4dd8cc0624c1b219f255b1e4adc7a62be1985037
Hong <tode1126@gmail.com>
2019-07-04 17:07:59 +0900

    restruant sql 파일

---
+++ Fourman-YGY-Project/src/main/java/spring/service/restraunt/RestrauntService.java
@@ -0,0 +1,12 @@
+package spring.service.restraunt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import spring.data.restraunt.RestrauntDao;
+
+public class RestrauntService {
+	@Autowired
+	private RestrauntDao dao;
+	
+	
+}
