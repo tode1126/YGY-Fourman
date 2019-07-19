8d8aeff27d675b664c504d4e1a17cdce398bd844
acorn <acorn@acorn-PC>
2019-07-19 17:20:59 +0900

    1. 게시판 출력 및 글쓰기,수정,삭제,좋아요 추가 2. 사진 추가 3. mvc-config2.xml 추가 4. web.xml 좋아요 부분 추가

--- Fourman-YGY-Project/src/main/java/spring/service/ReboardService.java
+++ Fourman-YGY-Project/src/main/java/spring/service/ReboardService.java
@@ -1,9 +1,12 @@
 package spring.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import spring.data.ReboardDao;
+import spring.data.ReboardDto;
 
 
 @Service
@@ -11,7 +14,47 @@ public class ReboardService {
 	@Autowired
 	private ReboardDao dao;
 	
+	public int getTotalCount() { return dao.getTotalCount(); }
+	 
+	public void insertReboard(ReboardDto dto)
+	{
+		 dao.insertReboard(dto);
+		 return;
+	}
 	
-	 public int getTotalCount() { return dao.getTotalCount(); }
+	public List<ReboardDto> getList(int offset,int block)
+	{
+		return dao.getList(offset, block);
+	}
+	
+	public void updateReadCount(int num)
+	{
+		dao.updateReadCount(num);
+	}
+	
+	public ReboardDto getData(int num)
+	{
+		return dao.getData(num);
+	}
+	
+	public void reboardUpdate(ReboardDto dto)
+	{
+		dao.reboardUpdate(dto);
+	}
+	
+	public void reboardDelete(int num)
+	{
+		dao.reboardDelete(num);
+	}
+	
+	public void reboardHappyUpdate(int num)
+	{
+		dao.reboardHappyUpdate(num);
+	}
+	
+	public int getSelectHappy(int num)
+	{
+		return dao.getSelectHappy(num);
+	}
 	
 }
